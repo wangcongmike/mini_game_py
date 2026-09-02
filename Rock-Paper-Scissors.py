2d44f91df55dc40544fe2a786c18b577bc03810b
wangcongmike <wangcongmike@163.com>
2026-09-02 11:01:30 +0800

    迭代：调用函数传参判断胜负

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,6 +1,19 @@
 #------"石头剪刀布"-----
 import random
-def Rock_Paper_Scissors():  # 定义函数用 "def"
+
+def Rock_Paper_Scissors(player_choice,computer_choice):
+    print(f"你选择的是{player_choice}",f"电脑的选择是{computer_choice}")
+    
+     # 判断胜负
+    if player_choice == computer_choice:
+        print("平局")
+    elif (player_choice == "石头" and computer_choice == "剪刀") or  \
+         (player_choice == "剪刀" and computer_choice == "布") or \
+         (player_choice == "布" and computer_choice == "石头"):
+        print("这局你赢了！")
+    else:    
+        print("你输了！")
+def main():  # 定义函数用 "def"
     choices = ["石头","剪刀","布"]  # 数组
     showhand = {"石头":"石头","剪刀":"剪刀","布":"布","r":"石头","s":"剪刀","p":"布"}   #用字典来映射简写
     
@@ -16,32 +29,17 @@ def Rock_Paper_Scissors():  # 定义函数用 "def"
         player_choice = showhand[player_input]
         computer_choice = random.choice(choices)
         
-        print(f"你选择的是{player_choice}",f"电脑的选择是{computer_choice}")
-        
-        # 判断胜负
-        if player_choice == computer_choice:
-            print("平局")
-        elif (player_choice == "石头" and computer_choice == "剪刀") or  \
-             (player_choice == "剪刀" and computer_choice == "布") or \
-             (player_choice == "布" and computer_choice == "石头"):
-            print("这局你赢了！")
-        else:    
-            print("你输了！")
+        # 调用函数传参判断胜负
+        Rock_Paper_Scissors(player_choice,computer_choice)
         
         # 询问是否再来一次
-        #again = input("再来一局?y/n:").strip().lower()
-        #if again != "y":     # 这里会有BUG，选除 y之外的字母一样会中断跳出整个程序；
-          #  print("再见,欢迎下次再来！")
-           # break
         while True:
             again = input("再来一局? y/n:").strip().lower()
             if again == "y":
                 break
             elif again == "n":
                 print("下次再来哈~")
-                #exit()   # exit() 是跳出整个代码，直接杀死进程，太暴力，尽量少用；
-                #break    # break  是跳出循环，但函数里出来循环外的代码还会执行
-                return    # return 是跳出函数，结束代码最优雅，也是最温和！
+                return   # return 是跳出函数，结束代码最优雅，也是最温和！break  是跳出循环，但函数里出来循环外的代码还会执行exit() 是跳出整个代码，直接杀死进程，太暴力，尽量少用；
             else:
                 print("输入错误，请输入 y/n: ")
         
@@ -49,6 +47,6 @@ def Rock_Paper_Scissors():  # 定义函数用 "def"
 if __name__ == "__main__":  
     # 这段代码是 Python 中最经典、最重要的入口点惯用法（Entry Point Idiom）。它的核心作用是：让同一个 .py 文件既能被直接运行，又能被安全地导入为模块，而不会在导入时自动执行游戏.
     # “如果这个脚本是被人直接点击运行或在命令行里执行的主程序，那么就去启动游戏；如果它是被别人引进来的‘工具包’，那就老老实实待着，别擅自开始玩。”
-    Rock_Paper_Scissors()
+    main()
     
         
